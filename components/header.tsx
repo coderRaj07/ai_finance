23a9e39661c20d50471692eaf736385e8252c6b2
coderRaj07 <12131501.edu@gmail.com>
2025-07-02 16:45:18 +0530

    feat: header color palatte updated

--- components/header.tsx
+++ components/header.tsx
@@ -16,54 +16,54 @@ const Header = () => {
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
           <Image
-            src={"/Logo.png"}       
+            src={"/Logo.png"}
             alt="Logo"
-            width={927}             
+            width={927}
             height={258}
             className='h-12 w-auto object-contain'
           />
         </Link>
-   
-     <div className='flex items-center gap-4'>
-      <SignedIn>
 
-        <Link href={"/dashboard"} >
-          <Button variant="outline" className='text-gray-600 hover:text-blue-600 flex items-center gap-2'>
-            <LayoutDashboard size={18}/>
-              <span className="hidden md:inline">
-                Dashboard
-              </span>
-          </Button>
-        </Link>
+        <div className='flex items-center gap-4'>
+          <SignedIn>
 
-        <Link href={"/transaction/create"}>
-          <Button className='flex items-center gap-2'>
-            <PenBox size={18}/>
-              <span className="hidden md:inline">
-                Add Transaction
-              </span>
-          </Button>
-        </Link>
+            <Link href={"/dashboard"} >
+              <Button variant="outline" className='border-[#004930] text-[#004930] hover:bg-[#cb9827] hover:text-white flex items-center gap-2'>
+                <LayoutDashboard size={18} />
+                <span className="hidden md:inline">
+                  Dashboard
+                </span>
+              </Button>
+            </Link>
 
-      </SignedIn>
+            <Link href={"/transaction/create"}>
+              <Button className='bg-[#004930] text-white hover:bg-[#cb9827] flex items-center gap-2'>
+                <PenBox size={18} />
+                <span className="hidden md:inline">
+                  Add Transaction
+                </span>
+              </Button>
+            </Link>
+
+          </SignedIn>
 
 
-      <SignedOut>
-        <SignInButton forceRedirectUrl="/dashboard">
-          <Button variant="outline">Login</Button>
-        </SignInButton>
-      </SignedOut>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
 
-      <SignedIn>
-        <UserButton 
-          appearance={{
-            elements:{
-              avatarBox: "w-10 h-10",
-            }
-          }}
-        />
-      </SignedIn>
-      </div>   
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                }
+              }}
+            />
+          </SignedIn>
+        </div>
       </nav>
     </div>
   );
