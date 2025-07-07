d41281b31df2c0b5488d4873519de79f253becbb
coderRaj07 <12131501.edu@gmail.com>
2025-07-07 21:12:06 +0530

    feat: user checkin feature added

---
+++ lib/checkUser.ts
@@ -0,0 +1,46 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "./prisma";
+
+export const checkUser = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+
+  try {
+
+    // If user exists in database return user
+    const loggedInUser = await db.user.findUnique({
+      where: {
+        clerkUserId: user.id,
+      },
+    });
+
+    if (loggedInUser) {
+      return loggedInUser;
+    }
+
+    // If user doesnot exist in database 
+    // Add him in the database
+
+    const name = `${user.firstName} ${user.lastName}`;
+
+    const newUser = await db.user.create({
+      data: {
+        clerkUserId: user.id,
+        name,
+        imageUrl: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+
+    return newUser;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    } else {
+      console.log("An unknown error occurred:", error);
+    }
+  }  
+};
